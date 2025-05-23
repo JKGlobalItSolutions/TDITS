d8f7d28162fb3d8182c03a85051d7c2be2d52775
sriram A <sriram.coder2004@gmail.com>
2025-05-23 12:02:00 +0530

    testng
    testng

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TDITS/', 
+  base: '/TDITS/', // 👈 Important!
 })
